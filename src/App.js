940cd070d0f28ad99565a4a1751222e0a7ef0a83
hyemin12 <khnm31@naver.com>
2022-12-19 16:10:32 +0900

    'api연결'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import logo from "./logo.svg";
+import axios from "axios";
+import { useEffect } from "react";
+
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 function App() {
+  const getData = async () => {
+    const res =
+      await axios.get(`https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=${KEY}
+    &part=snippet,contentDetails,statistics,status
+`);
+    console.log(res);
+  };
+  useEffect(() => {
+    getData();
+  });
   return <div className="App"></div>;
 }
 
