dcd81620b4fe1c49b6966a07ad30d0100423d56e
hyemin12 <khnm31@naver.com>
2023-02-02 15:47:47 +0900

    '이미지업로드실패시오류처리'

--- src/components/ChannelThumbnail.js
+++ src/components/ChannelThumbnail.js
@@ -7,11 +7,26 @@ import LinkButton from "./Button/LinkButton";
 // 채널 썸네일 - 이미지 컴포넌트
 const ChannelThumbnail = ({ customUrl, channelId, title, url, size, type }) => {
   const location = useLocation();
+  const handleImgError = (e) => {
+    e.target.src = unknownImg;
+  };
   if (location.pathname === "/channel" || type === "default")
-    return <Img src={url ? url : unknownImg} alt={title} size={size} />;
+    return (
+      <Img
+        src={url ? url : unknownImg}
+        onError={handleImgError}
+        alt={title}
+        size={size}
+      />
+    );
   return (
     <LinkButton pathname={"/channel"} query={customUrl} id={channelId}>
-      <Img src={url ? url : unknownImg} alt={title} size={size} />
+      <Img
+        src={url ? url : unknownImg}
+        onError={handleImgError}
+        alt={title}
+        size={size}
+      />
     </LinkButton>
   );
 };
