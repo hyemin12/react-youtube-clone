579d4d7235e02ad6a882fa3c642906648ad98c14
hyemin12 <khnm31@naver.com>
2023-02-03 18:13:10 +0900

    'url주소여부확인후a태그로감싸기'

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,20 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 
 // 설명 컴포넌트
 const Description = ({ des }) => {
+  const urlRegex = /(https?:\/\/[^ ]*)/;
+
   return (
     <div>
-      {des
-        .split("\n")
-        .map((sentence, idx) =>
-          sentence === "" ? (
-            <br key={idx} />
-          ) : (
-            <p key={`${sentence}${idx}`}>{sentence}</p>
-          )
-        )}
+      {des.split("\n").map((sentence, idx) => {
+        if (sentence === "") return <br key={idx} />;
+        if (sentence.match(urlRegex))
+          return sentence.split(urlRegex).map((word) =>
+            word.match(urlRegex) ? (
+              <Link href={word} target="_blank" rel="noreferrer">
+                {word}
+              </Link>
+            ) : word === "" ? (
+              <br />
+            ) : (
+              <span>{word}</span>
+            )
+          );
+        return <p key={`${sentence}${idx}`}>{sentence}</p>;
+      })}
     </div>
   );
 };
+
+const Link = styled.a`
+  color: blue;
+`;
 
 export default React.memo(Description);
